24285a22a9f1fa8fa3455db8c178cc6aca358f39
Gabor Nyeki <securior@gmail.com>
2018-12-23 00:01:10 -0500

    adding description to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='coeftable',
     version='1.0',
-    description=u'',
+    description=u'two command-line tools to generate arbitrarily formatted tables from JSON data files',
     author=u'Gabor Nyeki',
     url='http://www.gabornyeki.com/',
     packages=['coeftable'],
