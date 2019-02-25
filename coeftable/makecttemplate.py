0abc39056906e33d3da863d1d11492ebe8305326
Gabor Nyeki <securior@gmail.com>
2019-02-24 23:51:22 -0500

    makecttemplate.py: changing to new concept
    The new concept: treat each variable in a specification as a "block."  A
    block can draw multiple entries from its JSON input object.  A block can
    also take up multiple cells in the output table.
    
    What exactly a block draws from the input object, and how it is exactly
    displayed in cells, is specified in the "block template."

--- coeftable/makecttemplate.py
+++ coeftable/makecttemplate.py
@@ -1,8 +1,13 @@
 import argh
+import collections
 import re
 import operator as op
 import itertools as it
 import copy
+
+
+Block = collections.namedtuple("Block", ("instance_index", "path"))
+IndexedCell = collections.namedtuple("IndexedCell", ("instance_index", "value"))
 
 
 def consistent_item_count(*args):
@@ -32,35 +37,83 @@ assert into_list_if_not_None(1, None, 2) == [1, 2]
 assert into_list_if_not_None(None, None, None) == []
 
 
-def code_for_path(path, instance_index, spec_as_coefs):
-    if spec_as_coefs:
-        return "@@%d::%s@@" % (instance_index, path)
-    else:
-        return "%%(%d::%s).3f" % (instance_index, path)
+def adapt_instance_index(template, instance_index):
+    retval = ""
+    start_pos = 0
+
+    while 1:
+        template_chunk = template[start_pos:]
+
+        # This pattern matches the following strings:
+        #
+        # - %(1::N)s
+        # - %(1::N)d
+        # - %(1::coef::N::ci_l).3f
+        # - %(1::N::mean)07.2f
+        #
+        # The respective group pairs that it extracts are:
+        #
+        # - %(, ::N)s
+        # - %(, ::N)d
+        # - %(, ::coef::N::ci_l).3f
+        # - %(, ::N::mean)07.2f
+        #
+        m = re.search(
+                r"(%\()1(::[^)]+\)(?:[sd]|[0-9]*[.][0-9]+f))",
+                template_chunk)
+
+        if m is None:
+            retval += template_chunk
+            return retval
+
+        retval += template_chunk[:m.start()]
+        retval += ("%s%d%s"
+                   % (m.group(1),
+                      instance_index,
+                      m.group(2)))
+
+        start_pos += m.end()
 
 
-def parse_coef(code):
-    m = re.match(r"^@@(\d+)::(.+)@@$", code)
+def adapt(template, template_path, instance_index, path):
+    retval = ""
+    start_pos = 0
 
-    if m is None:
-        return None
+    while 1:
+        template_chunk = template[start_pos:]
 
-    return (m.group(1), m.group(2))
+        # This pattern matches, e.g., with template_path="N",
+        # the following strings:
+        #
+        # - %(1::N)s
+        # - %(1::N)d
+        # - %(1::coef::N::ci_l).3f
+        # - %(1::N::mean)07.2f
+        #
+        # The respective group pairs that it extracts are:
+        #
+        # - ::, )s
+        # - ::, )d
+        # - ::coef::, ::ci_l).3f
+        # - ::, ::mean)07.2f
+        #
+        m = re.search(
+                (r"%%\(1((?:::[^)]+)*::)%s((?:::[^)]+)*\)(?:[sd]|[0-9]*[.][0-9]+f))"
+                 % template_path),
+                template_chunk)
 
+        if m is None:
+            retval += template_chunk
+            return retval
 
-def is_coef(code):
-    return parse_coef(code) is not None
+        retval += template_chunk[:m.start()]
+        retval += ("%%(%d%s%s%s"
+                   % (instance_index,
+                      m.group(1),
+                      path,
+                      m.group(2)))
 
-
-def resolve_coef(code):
-    try:
-        a, b = parse_coef(code)
-    except TypeError:
-        return None
-
-    return ("%%(%s::coef::%s::est).3f" % (a, b),
-            "%%(%s::coef::%s::stars)s" % (a, b),
-            "%%(%s::coef::%s::se).4f" % (a, b))
+        start_pos += m.end()
 
 
 def is_float_format(code):
@@ -71,12 +124,11 @@ def is_float_format(code):
 
 def assemble_panels(
         spec_defs,
-        spec_as_coefs,
         var_labels,
         panel_defs,
         footer_rows,
-        footer_vars,
-        footer_var_labels):
+        footer_cells,
+        footer_cell_labels):
     """
     Returns a list of panels, each of which is a list of
     columns, each of which is a list of cells.
@@ -93,8 +145,8 @@ def assemble_panels(
             if footer_rows:
                 first_col += [row[0] for row in footer_rows]
 
-            if footer_var_labels:
-                first_col += footer_var_labels
+            if footer_cell_labels:
+                first_col += footer_cell_labels
 
         panel_cols = [first_col]
         col_index = 0
@@ -103,17 +155,18 @@ def assemble_panels(
             instance_index += 1
             col_index += 1
 
-            cells = [code_for_path(
-                         path, instance_index, spec_as_coefs)
+            cells = [Block(instance_index=instance_index,
+                           path=path)
                      if path else ""
                      for path in spec_defs[spec_id - 1]]
 
             if footer_rows:
                 cells += [row[col_index] for row in footer_rows]
 
-            if footer_vars:
-                cells += ["%%(%d::%s)s" % (instance_index, var)
-                          for var in footer_vars]
+            if footer_cells:
+                cells += [IndexedCell(instance_index=instance_index,
+                                      value=cell)
+                          for cell in footer_cells]
 
             panel_cols.append(cells)
 
@@ -140,7 +193,7 @@ def markdown_from(panels):
               "--output-as markdown is not implemented yet.")
 
 
-def latex_from(panels, without_coef_stars):
+def latex_from(panels, block_template, template_path, spec_as_coefs, without_coef_stars):
     n_cols = len(panels[0])
     n_rows = len(panels[0][0])
     n_panels = len(panels)
@@ -164,6 +217,9 @@ def latex_from(panels, without_coef_stars):
 
     for panel_index, panel in enumerate(panels):
         if n_panels > 1:
+            if panel_index > 0:
+                code += r"\midrule" + "\n"
+
             code += (r"& \multicolumn{%d}{c}{\em Panel %s} \\[1em]"
                      % (n_cols - 1, chr(65 + panel_index))) + "\n"
 
@@ -174,34 +230,63 @@ def latex_from(panels, without_coef_stars):
             # TODO Escape LaTeX special characters.
 
             this_line = ""
-            next_line = ""
 
-            if any(is_coef(cell) for cell in row[1:]):
+            if spec_as_coefs or len(block_template) == 2:
+                next_line = ""
+
+            if any(isinstance(cell, Block) for cell in row[1:]):
                 this_line += row[0]
 
                 for cell in row[1:]:
-                    if not is_coef(cell):
-                        this_line += " & %s" % cell
-                        next_line += " & "
-                    else:
-                        est, stars, se = resolve_coef(cell)
+                    if not cell:
+                        this_line += " & "
 
-                        if without_coef_stars:
-                            this_line += " & $%s$" % est
-                        else:
-                            this_line += " & $%s$%s" % (est, stars)
-                        next_line += " & ($%s$)" % se
+                        if spec_as_coefs or len(block_template) == 2:
+                            next_line += " & "
+                    elif spec_as_coefs:
+                        this_line += (" & $%%(%d::coef::%s::est).3f$"
+                                      % (cell.instance_index,
+                                         cell.path))
+                        if not without_coef_stars:
+                            this_line += ("%%(%d::coef::%s::stars)s"
+                                          % (cell.instance_index,
+                                             cell.path))
+
+                        next_line += (" & ($%%(%d::coef::%s::se).4f$)"
+                                      % (cell.instance_index,
+                                         cell.path))
+                    else:
+                        this_line += " & "
+                        this_line += adapt(
+                                         block_template[0],
+                                         template_path,
+                                         cell.instance_index,
+                                         cell.path)
+
+                        if len(block_template) == 2:
+                            next_line += " & "
+                            next_line += adapt(
+                                             block_template[1],
+                                             template_path,
+                                             cell.instance_index,
+                                             cell.path)
 
                 code += this_line + r" \\" + "\n"
-                code += next_line + r" \\[1em]" + "\n"
+
+                if spec_as_coefs or len(block_template) == 2:
+                    code += next_line + r" \\[1em]" + "\n"
             else:
                 this_line += row[0]
 
                 for cell in row[1:]:
-                    if is_float_format(cell):
-                        this_line += " & $%s$" % cell
+                    this_line += " & "
+
+                    if isinstance(cell, IndexedCell):
+                        this_line += adapt_instance_index(
+                                         cell.value,
+                                         cell.instance_index)
                     else:
-                        this_line += " & %s" % cell
+                        this_line += cell
 
                 code += this_line + r" \\" + "\n"
 
@@ -213,26 +298,37 @@ def latex_from(panels, without_coef_stars):
 
 @argh.arg("--define-spec", type=str, nargs="+", required=True, action="append")
 @argh.arg("--var-labels", type=str, nargs="+", required=True)
+@argh.arg("--block-template", type=str, nargs="+")
 @argh.arg("--add-panel", type=int, nargs="+", action="append")
 @argh.arg("--add-footer-row", type=str, nargs="+", action="append")
-@argh.arg("--add-footer-var", type=str, nargs="+")
-@argh.arg("--footer-var-labels", type=str, nargs="+")
+@argh.arg("--add-footer-cell", type=str, nargs="+")
+@argh.arg("--footer-cell-labels", type=str, nargs="+")
 @argh.arg("--output-as", type=str, required=True, choices=("csv", "markdown", "latex"))
 def dispatcher(
         define_spec=None,
+        var_labels=None,
+        block_template=None,
         spec_as_coefs=False,
         without_coef_stars=False,
-        var_labels=None,
         add_panel=None,
         add_footer_row=None,
-        add_footer_var=None,
-        footer_var_labels=None,
+        add_footer_cell=None,
+        footer_cell_labels=None,
         output_as=None):
-    if op.xor(add_footer_var is None,
-              footer_var_labels is None):
+    if op.xor(block_template is None,
+              spec_as_coefs):
+        raise argh.exceptions.CommandError(
+                  "Must specify either --block-template or "
+                  + "--spec-as-coefs (and not both).")
+    elif op.xor(add_footer_cell is None,
+                footer_cell_labels is None):
         raise argh.exceptions.CommandError(
                   "Must specify either both --add-footer-var "
                   + "and --footer-var-labels or neither.")
+    elif block_template is not None and len(block_template) > 2:
+        raise NotImplementedError(
+                  "--block-template can't have more than two "
+                  + "arguments.")
 
     if add_panel is None:
         add_panel = [[1]]
@@ -253,27 +349,30 @@ def dispatcher(
                   "Number of columns implied by panels and "
                   + "footer rows must be equal.")
 
+    if block_template:
+        template_path = define_spec[0][0]
+    else:
+        template_path = None
+
     panels = assemble_panels(
                  spec_defs=define_spec,
-                 spec_as_coefs=spec_as_coefs,
                  var_labels=var_labels,
                  panel_defs=add_panel,
                  footer_rows=add_footer_row,
-                 footer_vars=add_footer_var,
-                 footer_var_labels=footer_var_labels)
+                 footer_cells=add_footer_cell,
+                 footer_cell_labels=footer_cell_labels)
 
-    print panels
-
-    # TODO
-    # - Each of these views will need to be ready to resolve
-    #   @@*::*@@-type coefficient shorthands.
-    #
     if output_as == "csv":
         print csv_from(panels)
     elif output_as == "markdown":
         print markdown_from(panels)
     else:
-        print latex_from(panels, without_coef_stars)
+        print latex_from(
+                  panels,
+                  block_template,
+                  template_path,
+                  spec_as_coefs,
+                  without_coef_stars)
 
 
 def main():
